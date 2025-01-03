29bf5aeab17172f530e3c2b0c8c1728ba19b1ed5
fridoustin <fridolinaustin16@gmail.com>
2025-01-03 12:39:24 +0800

    feat: add font and colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFFF7B00);
+  static const Color secondaryColor = Color(0xFFFF8D21);
+  static const Color backgroundColor = Color(0xFFFFF4DF);
+  static const Color primaryBlack = Color(0xFF353535);
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/constants/theme.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter/constants/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final ThemeData appTheme = ThemeData(
+  scaffoldBackgroundColor: AppColors.backgroundColor,
+  primaryColor: AppColors.primaryColor,
+  useMaterial3: true,
+  colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+  primaryTextTheme: primaryTextTheme,
+  textTheme: primaryTextTheme
+);
+
+final TextTheme primaryTextTheme = GoogleFonts.montserratTextTheme().copyWith(
+  titleLarge: GoogleFonts.montserrat(
+    fontSize: 24,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryBlack,
+  ),
+  titleMedium: GoogleFonts.montserrat(
+    fontSize: 20,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryBlack,
+  ),
+  titleSmall: GoogleFonts.montserrat(
+    fontSize: 18,
+    fontWeight: FontWeight.w700,
+    color: AppColors.primaryBlack,
+  ),
+  labelLarge: GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryBlack,
+  ),
+  labelMedium: GoogleFonts.montserrat(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryBlack,
+  ),
+  labelSmall: GoogleFonts.montserrat(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    color: AppColors.primaryBlack,
+  ),
+  bodyLarge: GoogleFonts.montserrat(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryBlack,
+  ),
+  bodyMedium: GoogleFonts.montserrat(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryBlack,
+  ),
+  bodySmall: GoogleFonts.montserrat(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    color: AppColors.primaryBlack,
+  ),
+);
