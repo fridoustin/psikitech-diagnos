63e3cca3d23b52cd4574e41298b91bcfd80ec14b
fridoustin <fridolinaustin16@gmail.com>
2025-01-03 12:41:13 +0800

    feat: make app navigation

---
+++ lib/common/utils/app_nav.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppNavigation {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+  Future<dynamic> navigateTo(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState!
+        .pushNamed(routeName, arguments: arguments);
+  }
+
+  Future<dynamic> navigateReplace(String routeName, {dynamic arguments}) {
+    return navigatorKey.currentState!.pushNamedAndRemoveUntil(
+      routeName,
+      (Route<dynamic> route) => false,
+      arguments: arguments,
+    );
+  }
+
+  void goBack() {
+    return navigatorKey.currentState!.pop();
+  }
+}
