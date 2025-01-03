b6e7f95f4ddc03cb5256d9d984570ceb698ec1f4
fridoustin <fridolinaustin16@gmail.com>
2025-01-03 12:39:52 +0800

    feat: make result screen

---
+++ lib/features/diagnosis/ui/result_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:learn_flutter/common/widgets/appBar/app_bar_diagnos.dart';
+import 'package:learn_flutter/features/home/home_screen.dart';
+
+class ResultScreen extends StatelessWidget {
+  static const String route = '/result';
+  final String result;
+
+  const ResultScreen({super.key, required this.result});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AppBarDiagnosWidget(label: "Hasil Diagnosa"),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                result,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pushReplacementNamed(HomePage.route);
+                },
+                child: const Text('Kembali'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
